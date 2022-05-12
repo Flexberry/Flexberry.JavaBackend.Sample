0aefafece789edcfd0010630fb17056ac8c24d05
AntonIV8 <anton-iv87@yandex.ru>
2022-05-12 16:00:00 +0500

    Правка конфига фронтенда

--- changeFrontendConfig.sh
+++ changeFrontendConfig.sh
@@ -39,3 +39,33 @@ then
     IFS=$ifs
 	mv ./tests/dummy/config/new_environment.js ./tests/dummy/config/environment.js
 fi
+
+if [ -f "testem.js" ]
+then
+	echo "module.exports = {
+		   test_page: 'tests/index.html?hidepassed',
+			disable_watching: true,
+			launch_in_ci: [
+		      'Chromium'
+			],
+			launch_in_dev: [
+			 'Chromium'
+			],
+			browser_args: {
+		     Chromium: {
+				  ci: [
+					// --no-sandbox is needed when running Chrome inside a container
+					// process.env.CI ? '--no-sandbox' : null,
+					'--no-sandbox',
+					'--headless',
+					'--disable-gpu',
+					'--disable-dev-shm-usage',
+					'--disable-software-rasterizer',
+					'--mute-audio',
+					'--remote-debugging-port=0',
+					'--window-size=1440,900'
+				  ].filter(Boolean)
+				}
+			  }
+			};" > testem.js
+fi
