4270aa651d956370661ba723b28d547faef34c48
AntonIV8 <anton-iv87@yandex.ru>
2022-05-18 10:09:45 +0500

    Правки, убрал сборку и запуск фронтенда в докере

--- changeFrontendConfig.sh
+++ changeFrontendConfig.sh
@@ -2,7 +2,7 @@
 
 if [ -f "./tests/dummy/config/environment.js" ]
 then
-	back="var testODataServiceURL = 'http://localhost/EmberFlexberryDummyService/odata';"
+	back="var testODataServiceURL = 'http://localhost:8080/EmberFlexberryDummyService/odata';"
               
 	ifs=$IFS
 	IFS=
@@ -38,34 +38,4 @@ then
 	done < ./tests/dummy/config/environment.js > ./tests/dummy/config/new_environment.js
     IFS=$ifs
 	mv ./tests/dummy/config/new_environment.js ./tests/dummy/config/environment.js
-fi
-
-if [ -f "testem.js" ]
-then
-	echo "module.exports = {
-		   test_page: 'tests/index.html?hidepassed',
-			disable_watching: true,
-			launch_in_ci: [
-		      'Chromium'
-			],
-			launch_in_dev: [
-			 'Chromium'
-			],
-			browser_args: {
-		     Chromium: {
-				  ci: [
-					// --no-sandbox is needed when running Chrome inside a container
-					// process.env.CI ? '--no-sandbox' : null,
-					'--no-sandbox',
-					'--headless',
-					'--disable-gpu',
-					'--disable-dev-shm-usage',
-					'--disable-software-rasterizer',
-					'--mute-audio',
-					'--remote-debugging-port=0',
-					'--window-size=1440,900'
-				  ].filter(Boolean)
-				}
-			  }
-			};" > testem.js
 fi
--- create-image-frontend.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build --no-cache --file Dockerfile.Frontend -t emberflexberrydummy-java/ember-frontend .
